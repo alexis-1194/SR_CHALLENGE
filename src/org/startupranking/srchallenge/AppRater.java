d67cad7609e0f7fa4b7f867b271f41011de6484c
Alexis <alexis.sualv@gmail.com>
2021-03-04 13:23:37 -0500

    Agregando funcion(waitForLoad) para cargar la pagina antes automatizar

--- src/org/startupranking/srchallenge/AppRater.java
+++ src/org/startupranking/srchallenge/AppRater.java
@@ -1,31 +1,29 @@
 package org.startupranking.srchallenge;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.pagefactory.ByAll;
-
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class AppRater {
 
     private static WebDriver driver;
 
-    public static void launchBrowser(){
+    public static void launchBrowser() {
         String baseUrl = "https://apprater.net/add/";
-        System.setProperty("webdriver.chrome.driver",
-                System.getProperty("user.dir") +  "\\drivers\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\drivers\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.get(baseUrl);
         driver.manage().window().maximize();
 
     }
 
-    public static void send(String selectorType, String selector, String value){
+    public static void sendAfterValidate(String selectorType, String selector, String value) {
         switch (selectorType) {
             case "css":
                 driver.findElement(By.cssSelector(selector)).sendKeys(value);
@@ -42,34 +40,49 @@ public class AppRater {
         }
     }
 
-    public static void sendText(String label, String selectorType, String selector, String value){
+    public static void sendText(String label, String selectorType, String selector, String value) {
         try {
             Thread.sleep(1500);
             driver.findElements(By.tagName("fieldset")).clear();
         } catch (InterruptedException ex) {
             ex.printStackTrace();
         }
-        List<WebElement> list =  driver.findElements(By.tagName("label"));
+        List<WebElement> list = driver.findElements(By.tagName("label"));
         list.add(driver.findElement(By.tagName("textArea")));
 
         for (WebElement webE : list) {
             if (label.contentEquals(webE.getText())) {
-                send(selectorType, selector, value);
+                sendAfterValidate(selectorType, selector, value);
                 break;
             }
         }
+    }
+
+    public static boolean waitForLoad(WebDriver driver) {
+        ExpectedCondition<Boolean> pageLoadCondition = new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver driver) {
+                return ((JavascriptExecutor) driver).executeScript("return document.readyState").equals("complete");
+            }
+        };
+        WebDriverWait wait = new WebDriverWait(driver, 30);
+        return wait.until(pageLoadCondition);
     }
 
     public static void main(String[] args) throws InterruptedException {
 
         launchBrowser();
 
-        sendText("Your Name", "name", "user-submitted-name", "Lucas Ocampo");
-        sendText("Email Address", "css", "#usp_form > div:nth-child(3) > div:nth-child(2) > fieldset > input", "Lucas@Ocampo.com");
-        sendText("Product Title", "css", "#usp_form > fieldset.usp-title > input", "Prisma - AI that runs your phots into artwork in seconds");
-        sendText("Product URL", "xpath", "//*[@id=\"usp_form\"]/div[4]/fieldset/input", "https://apprater.net/add/Prisma");
-        sendText("Product Tags", "xpath", "//*[@id=\"usp_form\"]/div[5]/div[1]/fieldset/input", "game, Android, IA");
-
+        if (waitForLoad(driver)) {
+            sendText("Your Name", "name", "user-submitted-name", "Lucas Ocampo");
+            sendText("Email Address", "css", "#usp_form > div:nth-child(3) > div:nth-child(2) > fieldset > input",
+                    "Lucas@Ocampo.com");
+            sendText("Product Title", "css", "#usp_form > fieldset.usp-title > input",
+                    "Prisma - AI that runs your phots into artwork in seconds");
+            sendText("Product URL", "xpath", "//*[@id=\"usp_form\"]/div[4]/fieldset/input",
+                    "https://apprater.net/add/Prisma");
+            sendText("Product Tags", "xpath", "//*[@id=\"usp_form\"]/div[5]/div[1]/fieldset/input",
+                    "game, Android, IA");
+        }
 
     }
 }
